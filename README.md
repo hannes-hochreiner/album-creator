fcead4c98bbd5019b9ea11a69a5de0fe9cae694d
Hannes Hochreiner <hannes@hochreiner.net>
2022-09-24 09:22:08 +0200

    add processing parameter sets

--- README.md
+++ README.md
@@ -2,6 +2,35 @@
 
 A tool to process selected images.
 
+## Usage
+
+the tool can be used from the command line:
+
+```bash
+album-creator --conf config_file.json
+```
+
+## Parameters
+
+| Name | Description |
+| --- | --- |
+| config | absolute path of the configuration file |
+
+## Configuration file
+
+### Simple example
+```json
+{
+  "name": "test album",
+  "base": "/home/user/picture",
+  "images": [
+    { "filename": "picture1.jpg" },
+    { "filename": "picture2.jpg" },
+    { "filename": "picture3.jpg" }
+  ]
+}
+```
+
 ## License
 
 This work is licensed under the MIT license.
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,55 @@
 use anyhow::Result;
 use clap::Parser;
 use serde::Deserialize;
-use std::{fs, path::PathBuf, process::Command};
+use std::{collections::HashMap, fmt::Display, fs, path::PathBuf, process::Command};
 use uuid::Uuid;
 
 #[derive(Debug, Deserialize, PartialEq)]
 struct Album {
     name: String,
     base: String,
-    images: Vec<String>,
+    transformations: Option<HashMap<String, Vec<Transformation>>>,
+    images: Vec<Image>,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Clone)]
+enum Transformation {
+    Size { width: u32, height: u32 },
+    Normalize,
+    Enhance,
+    Unsharp { radius: u32 },
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct Image {
+    filename: String,
+    transformations: Option<String>,
+}
+
+impl Image {
+    pub fn to_string(&self) -> String {
+        self.filename.clone()
+    }
+}
+
+impl Display for Image {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.filename)
+    }
+}
+
+struct ParameterSet {
+    input: String,
+    output: String,
+    transformations: Vec<Transformation>,
 }
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-   /// Path of the configuration file
-   #[clap(short, long, value_parser)]
-   conf: String,
+    /// Path of the configuration file
+    #[clap(short, long, value_parser)]
+    conf: String,
 }
 
 fn main() -> Result<()> {
@@ -32,22 +65,31 @@ fn main() -> Result<()> {
         .ok_or(anyhow::anyhow!("could not generate output base path"))?;
 
     let album: Album = serde_json::from_str(&document).unwrap();
-    let filelist = prepare_filelists(&album, &out_base)?;
+    let parameters = prepare_parameters(&album, &out_base)?;
 
     fs::create_dir(out_base)?;
-    filelist
+    parameters
         .iter()
-        .map(|(in_file, out_file)| {
-            Command::new("gm")
-                .arg("convert")
-                .arg("-size")
-                .arg("1920x1080")
-                .arg("-normalize")
-                .arg("-enhance")
-                .arg("-unsharp")
-                .arg("3")
-                .arg(in_file)
-                .arg(out_file)
+        .map(|parameter_set| {
+            let mut com = Command::new("gm");
+
+            com.arg("convert");
+
+            for trans in &parameter_set.transformations {
+                match trans {
+                    Transformation::Enhance => com.arg("-enhance"),
+                    Transformation::Normalize => com.arg("-normalize"),
+                    Transformation::Size { width, height } => {
+                        com.arg("-size").arg(format!("{}x{}", width, height))
+                    }
+                    Transformation::Unsharp { radius } => {
+                        com.arg("-unsharp").arg(format!("{}", radius))
+                    }
+                };
+            }
+
+            com.arg(parameter_set.input.clone())
+                .arg(parameter_set.output.clone())
                 .output()?;
             Ok(())
         })
@@ -56,10 +98,10 @@ fn main() -> Result<()> {
         .arg("--new-window")
         .arg(out_base)
         .output()?;
-    filelist
+    parameters
         .iter()
-        .map(|(_, out_file)| {
-            fs::remove_file(out_file)?;
+        .map(|parameter_set| {
+            fs::remove_file(parameter_set.output.clone())?;
             Ok(())
         })
         .collect::<anyhow::Result<Vec<()>>>()?;
@@ -68,9 +110,27 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn prepare_filelists(album: &Album, out_base: &str) -> Result<Vec<(String, String)>> {
+fn prepare_parameters(album: &Album, out_base: &str) -> Result<Vec<ParameterSet>> {
     let max_len = get_max_length(album.images.len());
+    let mut transformations: HashMap<String, Vec<Transformation>> = match &album.transformations {
+        Some(trans) => (*trans).clone(),
+        None => HashMap::new(),
+    };
 
+    if !transformations.contains_key("default") {
+        transformations.insert(
+            String::from("default"),
+            vec![
+                Transformation::Size {
+                    width: 1920,
+                    height: 1080,
+                },
+                Transformation::Normalize,
+                Transformation::Enhance,
+                Transformation::Unsharp { radius: 3 },
+            ],
+        );
+    }
     album
         .images
         .iter()
@@ -79,7 +139,7 @@ fn prepare_filelists(album: &Album, out_base: &str) -> Result<Vec<(String, Strin
             let mut in_path = PathBuf::new();
 
             in_path.push(album.base.clone());
-            in_path.push(elem);
+            in_path.push(elem.to_string());
 
             let in_path = in_path
                 .to_str()
@@ -98,8 +158,14 @@ fn prepare_filelists(album: &Album, out_base: &str) -> Result<Vec<(String, Strin
                 .to_str()
                 .ok_or(anyhow::anyhow!("could not generate output path"))?;
 
-            Ok((in_path.to_string(), out_path.to_string()))
-
+            Ok(ParameterSet {
+                input: in_path.to_string(),
+                output: out_path.to_string(),
+                transformations: match &elem.transformations {
+                    Some(trans) => transformations[trans].clone(),
+                    None => transformations["default"].clone(),
+                },
+            })
         })
         .collect()
 }
@@ -123,4 +189,29 @@ fn get_max_length_1() {
     assert_eq!(get_max_length(5), 1);
     assert_eq!(get_max_length(14), 2);
     assert_eq!(get_max_length(3472), 4);
+}
+
+#[test]
+fn prepare_parameters_1() {
+    let mut trans = HashMap::new();
+
+    trans.insert(
+        String::from("default"),
+        vec![Transformation::Normalize, Transformation::Enhance],
+    );
+
+    let album = Album {
+        base: String::from("/temp_in/"),
+        name: String::from("test name"),
+        images: vec![Image {
+            filename: String::from("image 1"),
+            transformations: None,
+        }],
+        transformations: Some(trans),
+    };
+
+    let res = prepare_parameters(&album, "/tmp_out/").unwrap();
+
+    assert_eq!(res.len(), 1);
+    assert_eq!(res[0].transformations.len(), 2);
 }
